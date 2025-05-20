467911af07645f41006c103dd6e8d9f0b5d94e40
Julien Barbot <jubarbot@cisco.com>
2025-05-20 11:11:15 +0200

    Remove non used QueryEndpointSelection
    Signed-off-by: Julien Barbot <jubarbot@cisco.com>

--- plugins/agent_bridge_endpoint_selection.go
+++ plugins/agent_bridge_endpoint_selection.go
@@ -7,17 +7,6 @@ import "fmt"
 
 const NBRESULT = 1
 
-type QueryEndpointSelectionMatch struct {
-	Query       string  `json:"query"`
-	OperationId string  `json:"operationId,omitempty"`
-	Relevance   float64 `json:"relevance,omitempty"`
-	Error       string  `json:"error,omitempty"`
-}
-
-type QueryEndpointSelectionReply struct {
-	Results []QueryEndpointSelectionMatch `json:"results"`
-}
-
 func findSelectOperation(apiId string, input string) (*string, float64, error) {
 	apiSpecIndicesLock.RLock()
 	apiSpecIndex, present := apiSpecIndices[apiId]
@@ -53,30 +42,4 @@ func findSelectOperation(apiId string, input string) (*string, float64, error) {
 		}
 		return &results[0].Value, results[0].Relevance, nil
 	}
-}
-
-func selectEndpoint(apiID string, selectionQueries []string) []QueryEndpointSelectionMatch {
-	matches := make([]QueryEndpointSelectionMatch, len(selectionQueries))
-	for _, selectionQuery := range selectionQueries {
-		if len(selectionQuery) == 0 {
-			continue
-		}
-
-		matchingOperation, matchingScore, err := findSelectOperation(apiID, selectionQuery)
-
-		match := QueryEndpointSelectionMatch{
-			Query:     selectionQuery,
-			Relevance: matchingScore,
-		}
-		if matchingOperation != nil {
-			match.OperationId = *matchingOperation
-		}
-		if err != nil {
-			match.Error = err.Error()
-		} else {
-			match.Error = "ok"
-		}
-		matches = append(matches, match)
-	}
-	return matches
 }
